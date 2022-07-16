e3ea077ca106ec0c5af7b55cbf965b1591b53262
Hiaguedes <hiagorguedes@gmail.com>
2022-07-15 21:30:23 -0300

    ci: configurando cobertura de test

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,10 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    verbose: true
+    verbose: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov']
 };
 
 export default config;
